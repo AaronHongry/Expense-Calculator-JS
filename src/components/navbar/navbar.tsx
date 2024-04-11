79bf2b3b22a3e266559b59efdfe8a8c178438940
Aaron Hong <46896330+AaronHongry@users.noreply.github.com>
2024-04-11 14:37:43 -0400

    Completed Drop down menu

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -6,20 +6,20 @@ export const Navbar: React.FC = () => {
   return (
     <nav className="bg-green-600 p-4 drop-shadow-lg">
       <div className="container mx-auto py-2 flex justify-between items-center">
-        <div className="text-slate-50 text-lg font-bold"><Link href="/">Expense Calculator</Link></div>
+        <div className="text-slate-50 text-lg font-bold"><Link tabIndex={-1} href="/">Expense Calculator</Link></div>
         <ul className="flex items-center space-x-4">
           <li>
-            <Link href="/" className="text-slate-50 hover:text-gray-300">
+            <Link tabIndex={-1} href="/" className="text-slate-50 hover:text-gray-300">
               Home
             </Link>
           </li>
           <li>
-            <Link href="/about" className="text-slate-50 hover:text-gray-300">
+            <Link tabIndex={-1} href="/about" className="text-slate-50 hover:text-gray-300 pr-2">
               About
             </Link>
           </li>
           <li>
-            <NavbarAuth/>
+            <NavbarAuth />
           </li>
         </ul>
       </div>
--- src/components/navbar/navbarAuth.tsx
+++ src/components/navbar/navbarAuth.tsx
@@ -20,11 +20,13 @@ import {
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { addDoc, doc, setDoc } from "firebase/firestore";
+import React, { useState } from 'react';
 
 export const NavbarAuth: React.FC<{}> = () => {
 	const googleProvider = new GoogleAuthProvider();
 
 	const [user, loading] = useAuthState(auth);
+    const [pfp, setPfp] = useState<string | null>("");
 
 	const authenticate = async () => {
 		const result = await signInWithPopup(auth, googleProvider);
@@ -38,34 +40,34 @@ export const NavbarAuth: React.FC<{}> = () => {
 				userid: result.user.uid
 			});
 		}
+        setPfp(result.user.photoURL);
 	};
+
 	if (loading) return <></>;
 	if (!user)
 		return (
 			<div className="">
-				<button className="px-4 py-2 bg-slate-50 text-green-600 font-semibold rounded-xl transition duration-200 ease-out hover:bg-green-600 hover:text-slate-50 hover:outline hover:outline-2 hover: outline-slate-50" onClick={authenticate}>Login or Register</button>
+				<button tabIndex={-1} className="px-4 py-4 bg-slate-50 text-green-600 font-semibold rounded-xl transition duration-200 ease-out hover:bg-green-600 hover:text-slate-50 hover:outline hover:outline-2 hover:outline-slate-50" onClick={authenticate}>Login or Register</button>
 			</div>
 		);
 	return (
 		<div className="flex flex-col items-end">
 			<DropdownMenu>
-				<DropdownMenuTrigger>
-					<div className="flex outline outline-2  px-4 py-2 items-center gap-2">
-						<div className="text-md text-slate-50">{user.displayName}</div>
-						<Avatar>
-                            <AvatarImage src={user.photoURL || undefined}/>
-                            
+				<DropdownMenuTrigger tabIndex={-1} className="focus:outline-none">
+					<div className="flex px-4 py-2 items-center gap-2 text-green-600 bg-slate-50 rounded-xl transition duration-200 ease-out hover:bg-green-600 hover:outline hover:outline-2 hover:text-slate-50 hover:outline-slate-50 hover-border-avatar">
+						<div className="text-md font-semibold transition duration-200 ease-out">{user.displayName}</div>
+                        <Avatar id="avatar-component" className="border-2 border-green-600 transition duration-200 ease-out">
+                            <AvatarImage src={pfp || undefined}/>
                             <AvatarFallback className="bg-slate-400 text-white">
-                                {(user.displayName[0])}
-							</AvatarFallback>
-						</Avatar>
+                                {(user.displayName?.[0])}
+                            </AvatarFallback>
+                        </Avatar>
 					</div>
 				</DropdownMenuTrigger>
-				<DropdownMenuContent>
-					<DropdownMenuLabel>My Account</DropdownMenuLabel>
-					<DropdownMenuSeparator />
-					<Link href={`../profile/${user.uid}`}><DropdownMenuItem>My Expenses</DropdownMenuItem></Link>
-					<DropdownMenuItem onClick={() => signOut(auth)}>
+				<DropdownMenuContent className="bg-slate-50 w-44">
+					<Link href={`../profile/${user.uid}`}><DropdownMenuItem className="focus:bg-slate-200">My Expenses</DropdownMenuItem></Link>
+                    <DropdownMenuSeparator className="bg-slate-300" />
+					<DropdownMenuItem className="focus:bg-slate-200" onClick={() => signOut(auth)}>
 						Logout
 					</DropdownMenuItem>
 				</DropdownMenuContent>
