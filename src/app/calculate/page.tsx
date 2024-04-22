b425aa4bd3963fe8a9a1667e4dc925935a1c654a
Aaron Hong <46896330+AaronHongry@users.noreply.github.com>
2024-04-22 14:32:26 -0400

    Added Add Person funcitonality

--- src/app/calculate/page.tsx
+++ src/app/calculate/page.tsx
@@ -10,18 +10,41 @@ import {
 import { Separator } from "@/components/ui/separator";
 import React, { useState, useEffect } from "react";
 import AddItem from "@/components/addItem";
+import AddPerson from "@/components/addPerson";
 
 interface ExpenseProps {
   name: string;
   amount: number;
+  people: string[];
 }
 
-function Expense({name, amount}: ExpenseProps) {
+function Expense({name, amount, people}: ExpenseProps) {
   return (
-     <div className="flex flex-row justify-between py-2 w-full">
-      <p className="text-md text-slate-950">{name}</p>
-      <p className="text-md text-slate-950">${amount.toFixed(2)}</p>
+     <div className="grid grid-cols-5 gap-4 py-2 w-full">
+      <p className="text-md text-slate-950 col-span-1 font-semibold">{name}</p>
+      <Separator orientation="vertical" className="bg-slate-300 col-span-1"/>
+      <div className="col-span-1 justify-self-center">
+        {people.map((person, index) => (
+            <React.Fragment key={index}>
+              <span className="text-md text-slate-500 inline-block">{person}</span>
+              {(index != people.length - 1) && <span className="text-slate-500">, </span>}
+            </React.Fragment>
+          ))}
+      </div>
+      <Separator orientation="vertical" className="bg-slate-300 col-span-1 justify-self-end"/>
+      <p className="text-md text-slate-950 col-span-1 justify-self-end font-semibold">${amount.toFixed(2)}</p>
     </div> 
+  );
+}
+
+interface PeopleProps {
+  name: string;
+  amount: number;
+}
+
+function People({name, amount}: PeopleProps) {
+  return (
+    <div></div>
   );
 }
 
@@ -34,15 +57,31 @@ export default function Calculate() {
       { name: "Fries", amount: 15.99}
     ]
   );
+  const [people, setPeople] = useState(
+    [
+      "Aaron",
+      "Brian",
+      "Andrew"
+    ]
+  );
+
   const [isAddItemOpen, setIsAddItemOpen] = useState(false);
+  const [isAddPersonOpen, setIsAddPersonOpen] = useState(false);
 
   const handleOpenAddItem = () => setIsAddItemOpen(true);
   const handleCloseAddItem = () => setIsAddItemOpen(false);
 
+  const handleOpenAddPerson = () => setIsAddPersonOpen(true);
+  const handleCloseAddPerson = () => setIsAddPersonOpen(false);
 
   const handleAddItem = (name: string, amount: number) => {
     setExpenses(prevItems => [...prevItems, {name, amount}])
     handleCloseAddItem();
+  };
+
+  const handleAddPerson = (name: string) => {
+    setPeople(prevItems => [...prevItems, name]);
+    handleCloseAddPerson();
   };
 
   useEffect(() => {
@@ -65,7 +104,7 @@ export default function Calculate() {
 
             {expenses.map((expense, index) => (
               <React.Fragment key={index}>
-                <Expense key={index} name={expense.name} amount={expense.amount}/>
+                <Expense key={index} name={expense.name} amount={expense.amount} people={people}/>
                 <Separator className="bg-slate-200"/>
               </React.Fragment>
             ))}
@@ -93,7 +132,8 @@ export default function Calculate() {
 
           <CardContent className="flex flex-col justify-center items-center">
             <CardDescription className="text-slate-400 w-full text-center pb-2">There are no people in the bill!</CardDescription>
-            <button className="px-4 py-2 w-3/5 bg-green-600 text-slate-50 rounded-3xl hover:bg-green-500 transition ease-out duration-200">Add Person</button>
+            <button className="px-4 py-2 w-3/5 bg-green-600 text-slate-50 rounded-3xl hover:bg-green-500 transition ease-out duration-200" onClick={handleOpenAddPerson}>Add Person</button>
+            {isAddPersonOpen && <AddPerson isOpen={isAddPersonOpen} onConfirm={handleAddPerson} onCancel={handleCloseAddPerson}/>}
           </CardContent>
         </Card>
       </div>
---
+++ src/components/addPerson.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useState } from "react";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card";
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { CornerDownLeft } from 'lucide-react';
+
+interface addPersonProps {
+    isOpen: boolean;
+    onConfirm: (name: string) => void;
+    onCancel: () => void;
+}
+
+const AddPerson: React.FC<addPersonProps> = ({ isOpen, onConfirm, onCancel }) => {
+    const [name, setName] = useState('');
+
+    const handleFormSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        if (name) {
+            onConfirm(name);
+        }
+    };
+
+    return (
+        <>
+            {isOpen && (
+                <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-50">
+                    <Card className="bg-white rounded-xl">
+                        <CardHeader>
+                            <CardTitle>Add Person</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <form onSubmit={handleFormSubmit} autoComplete="off">
+                                <div className="flex flex-col gap-4">
+                                    <div>
+                                        <Label>Name</Label>
+                                        <Input type="text" placeholder="Name" className="rounded-xl border-slate-300 placeholder:text-slate-300" onChange={e => setName(e.target.value)}></Input>
+                                    </div>
+                                </div>
+                                <div className="flex pt-10 gap-2">
+                                    <button onClick={onCancel} className="w-1/4 px-4 py-2 bg-gray-300 rounded hover:bg-gray-200 focus:outline-none">
+                                        <CornerDownLeft className="text-gray-500"/>
+                                    </button>
+                                    <button type="submit" className="w-3/4 px-4 py-2 bg-green-600 text-slate-50 rounded hover:bg-green-500 transition ease-out duration-200">
+                                        Add Person
+                                    </button>
+                                </div>
+                            </form>
+                        </CardContent>
+                    </Card>
+                </div>
+            )}
+        </>
+    );
+}
+
+
+export default AddPerson;
